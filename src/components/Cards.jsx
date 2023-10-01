2bae5f60ccbc20edd81d090fd893551e6acb4d12
Luchopaulucci <luchopaluch@gmail.com>
2023-10-01 01:32:38 -0300

    :sparkles:Se agregan cards dinamicas en characters y paginacion

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,13 @@
-import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
+import {Card, CardHeader, CardFooter, Image, Button} from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
 
-const Cards = () => {
+const Cards = ({name, gender, url}) => {
   return (
     <Card isFooterBlurred className="w-72 h-80 col-span-12 sm:col-span-7">
     <CardHeader className="absolute z-10 top-1 flex-col items-start">
-      <p className="text-tiny text-white/60 uppercase font-bold">HUMAN</p>
-      <h4 className="text-white/90 font-medium text-xl">Luke Skywalker</h4>
+      <p className="text-tiny text-white/60 uppercase font-bold">{gender}</p>
+      <h4 className="text-white/90 font-medium text-xl">{name}</h4>
     </CardHeader>
     <Image
       removeWrapper
@@ -15,7 +16,7 @@ const Cards = () => {
       src="/public/images/luke.jpg"
     />
     <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-900 dark:border-default-100">
-      <Button radius="full" size="sm">Get more info</Button>
+      <Link radius="full" size="sm" to={"/characterInfo/" + url}>Get more info</Link>
     </CardFooter>
   </Card>
   )
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,25 +1,87 @@
-import Cards from "../components/Cards"
-import { Input } from "@nextui-org/react";
+import React, { useEffect, useState } from "react";
+import Cards from "../components/Cards";
+import { Input, Skeleton, Card, Pagination, PaginationItem, PaginationCursor } from "@nextui-org/react";
+import axios from "axios";
 
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+  const [page, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const skeletons = [];
+
+  useEffect(() => {
+    fetchCharacters(page);
+  }, [page]);
+
+  const fetchCharacters = (pageNumber) => {
+    setLoading(true);
+    axios
+      .get(`https://swapi.dev/api/people/?page=${pageNumber}`)
+      .then((response) => {
+        setCharacters(response.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const generateSkeletons = (count) => {
+    for (let i = 0; i < count; i++) {
+      skeletons.push(
+        <Card isFooterBlurred className="w-72 h-80 col-span-12 sm:col-span-7" key={i}>
+          <Skeleton className="z-0 w-full h-full object-cover" />
+        </Card>
+      );
+    }
+    return skeletons;
+  };
+
   return (
     <div className="my-10 flex justify-center items-center flex-col min-h-full sm:min-h-screen min-w-full">
       <div className="w-72">
-        <Input size="md" type="character" label="Character" placeholder="Enter the character to find" />
+        <Input
+          size="md"
+          type="character"
+          label="Character"
+          placeholder="Enter the character to find"
+        />
       </div>
-      <div className="flex flex-wrap flex-row justify-evenly gap-4 mt-20 w-3/4">
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+      <div className="flex flex-wrap flex-row justify-evenly gap-4 mt-20 md:w-3/4 lg:w-2/4">
+        {loading ? generateSkeletons(10) : characters.map((character, index) => (
+          <Cards
+            key={character.url}
+            name={character.name}
+            gender={character.gender}
+            url={character.url}
+          />
+        ))}
+      </div>
+      <div className="mt-20">
+        <Pagination
+          total={9}
+          onChange={handlePageChange}
+          initialPage={page}
+          showControls
+        >
+          {(currentPage, isActive) => (
+            <PaginationItem
+              key={currentPage}
+              active={isActive}
+            >
+              {currentPage}
+            </PaginationItem>
+          )}
+          <PaginationCursor />
+        </Pagination>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Characters
+export default Characters;
